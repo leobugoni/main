3464e5c82c5318c465a36b0fd8da3e571513fc4e
leooferreiraa <leoferreira@unochapeco.edu.br>
2017-06-16 15:59:16 -0300

    placar criad

--- projects/fast-typer/public/js/main.js
+++ projects/fast-typer/public/js/main.js
@@ -1,27 +1,20 @@
-var campo = $(".campo-digitacao");
+let campo = $(".campo-digitacao");
 
 $(function(){
-  $("#botao-salvar-frase").click(insereFraseDesafio);
-  $("#botao-salvar-frase").click(atualizaTamanhoFrase);
-  $("#botao-salvar-frase").click(inicializaCorretor);
-  $("#botao-salvar-tempo").click(insereTempoDesafio);
-  $("#botao-salvar-tempo").click(inicializaCronometro);
-  $("#botao-salvar-tempo").click(atualizaTamanhoFrase);
-  $("#botao-salvar-tempo").click(insereFraseDesafio);
-  insereTempoDesafio();
-  insereFraseDesafio();
-  atualizaTamanhoFrase();
+  $("#botao-salvar").click(iniciaJogo);
+
   inicializaContadores();
-  reiniciaJogo();
-  inicializaCronometro();
+
+
   $("#botao-reiniciar").click(reiniciaJogo);
+
   campo.attr("placeholder", "Insira uma frase e defina o tempo para começar");
 });
 
 function atualizaTamanhoFrase() {
-  var frase = $(".frase").text();
-  var numPalavras  = frase.split(" ").length;
-  var tamanhoFrase = $("#tamanho-frase").text();
+  let frase = $(".frase").text();
+  let numPalavras  = frase.split(" ").length;
+  let tamanhoFrase = $("#tamanho-frase").text();
   $("#tamanho-frase").text(numPalavras);
   //TODO nao altera numero de palavras pra 0 quando vazio
   /*if (tamanhoFrase) {
@@ -30,50 +23,52 @@ function atualizaTamanhoFrase() {
 }
 
 function insereFraseDesafio(){
-  var fraseDesafio = $("#frase-digitar").val();
+  let fraseDesafio = $("#frase-digitar").val();
   $(".frase").text(fraseDesafio);
 }
 function insereTempoDesafio(){
-  var tempoDesafio = $("#tempo-jogo").val();
+  let tempoDesafio = $("#tempo-jogo").val();
   $("#tempo-digitacao").text(tempoDesafio);
 }
 
 function inicializaContadores() {
   campo.on("input", function() {
-    var conteudo = campo.val();
+    let conteudo = campo.val();
 
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    let qtdPalavras = conteudo.split(/\S+/).length - 1;
     $("#contador-palavras").text(qtdPalavras);
 
-    var qtdCaracteres = conteudo.length;
+    let qtdCaracteres = conteudo.length;
     $("#contador-caracteres").text(qtdCaracteres);
 
   });
 }
 
 function inicializaCronometro() {
-  var tempoRestante = $("#tempo-jogo").val();
+  let tempoRestante = $("#tempo-jogo").val();
   if(tempoRestante){
   campo.one("focus", function() {
-    var cronometroID = setInterval(function() {
+    inicializaCorretor();
+    let cronometroID = setInterval(function() {
       tempoRestante--;
       $("#tempo-digitacao").text(tempoRestante);
       if (tempoRestante < 1) {
-        campo.attr("disabled", true);
         clearInterval(cronometroID);
-        campo.val("");
-        campo.attr("placeholder", "TEMPO ESGOTADO");
+        console.log("entrou");
+        finalizaJogo();
       }
     }, 1000);
+
   });
   }
 }
 
+
 function inicializaCorretor() {
-  var frase = $(".frase").text();
+  let frase = $(".frase").text();
   campo.on("input", function() {
-      var digitado = campo.val();
-      var comparavel = frase.substr(0 , digitado.length);
+      let digitado = campo.val();
+      let comparavel = frase.substr(0 , digitado.length);
 
       if(digitado == comparavel) {
           campo.addClass("campo-correto");
@@ -85,14 +80,68 @@ function inicializaCorretor() {
   });
 }
 
-function reiniciaJogo(){
-  campo.attr("disabled" ,false);
+function iniciaJogo(){
+  insereFraseDesafio();
+  insereTempoDesafio();
+  atualizaTamanhoFrase();
+  insereTempoDesafio();
+  inicializaContadores();
+  inicializaCronometro();
   campo.val("");
-  $("#contador-palavras").text("0");
-  $("#contador-caracteres").text("0");
+  campo.attr("disabled" ,false);
+  campo.attr("placeholder", "Insira uma frase e defina o tempo para começar");
+}
+
+function reiniciaJogo(){
   inicializaCronometro();
   insereTempoDesafio();
+  $("#contador-palavras").text("0");
+  $("#contador-caracteres").text("0");
+  campo.val("");
+  campo.attr("disabled" ,false);
   campo.removeClass("campo-correto");
   campo.removeClass("campo-errado");
   campo.attr("placeholder", "Insira uma frase e defina o tempo para começar");
 }
+
+function pausaJogo() {
+  //TODO fazer função para pausar o jogo
+}
+
+function finalizaJogo(){
+  campo.attr("disabled", true);
+  campo.val("");
+  campo.attr("placeholder", "TEMPO ESGOTADO");
+  inserePlacar();
+}
+
+function inserePlacar(){
+  let corpoTabela = $(".placar").find("tbody");
+  let usuario = "Leonardo";
+  let numPalavras = $("#contador-palavras").text();
+  let botaoExcluir = "<a href='#''><i class='small material-icons'>delete</i></a>";
+  let linha = novaLinha();
+
+  corpoTabela.prepend(linha);
+}
+
+function novaLinha(usuario, palavras) {
+  let linha = $("<tr>");
+  let colunaUsuario = $("<td>").text(usuario);
+  let colunaPalavra = $("<td>").text(palavras);
+  let colunaExcluir = $("<td>");
+  let link = $("<a>").addClass("botao-excluir").attr("href", "#");
+  let icone = $("<i>").addClass("small material-icons").text("delete");
+  link.append(icone);
+  colunaExcluir.append(link);
+  linha.append(colunaUsuario);
+  linha.append(colunaPalavra);
+  linha.append(colunaExcluir);
+
+}
+
+
+$(".botao-excluir").click(function(event){
+  event.preventDefault();
+  $(this).parent().parent().remove();
+});
